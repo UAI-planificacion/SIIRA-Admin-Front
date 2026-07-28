3a29607d2514e0d5af21b9681487c8057ab51fc8
kevinkeyssx <keyss1700@gmail.com>
2026-07-28 19:16:37 -0400

    Import implemented

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { JSX } from 'react';
+
 import { ProcessConfigView } from '@/components/process-configs/process-config-view';
 
 
-export default function DashboardPage(): React.JSX.Element {
+export default function DashboardPage(): JSX.Element {
     return (
         <div className="w-full max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8">
 			<ProcessConfigView />
