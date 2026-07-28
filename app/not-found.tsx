5df153b62865e610596c0306917da59df879c72c
kevinkeyssx <keyss1700@gmail.com>
2026-07-27 22:11:18 -0400

    Not found message updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import Image            from 'next/image';
+import { useRouter }    from 'next/navigation';
+
 import { Button } from '@/components/ui/button';
-import Image        from 'next/image';
-import { useRouter } from 'next/navigation';
+
 
 export default function Error404(): React.JSX.Element {
 	const router = useRouter();
 
 	return (
-		<section className="w-full max-w-2xl mx-auto my-auto py-16 px-4 flex flex-col items-center justify-center text-center gap-8">
+		<section className="w-full max-w-2xl mx-auto py-16 px-4 flex flex-col items-center justify-center text-center gap-8">
 			{/* Contenedor de Imágenes */}
 			<div className="flex flex-col sm:flex-row items-center justify-center gap-8">
 				{/* Imagen 404 */}
@@ -37,7 +39,7 @@ export default function Error404(): React.JSX.Element {
 			{/* Textos */}
 			<div className="space-y-4 max-w-xl">
 				<h1 className="text-3xl sm:text-4xl font-extrabold tracking-tight text-foreground">
-					Parece que has encontrado la puerta para llegar a donde querias ☹️
+					Parece que no has encontrado la página que buscabas ☹️
 				</h1>
 				<p className="text-muted-foreground text-base sm:text-lg">
 					Lo sentimos por eso! Visite nuestra página de inicio para llegar a donde necesita ir.
@@ -54,7 +56,3 @@ export default function Error404(): React.JSX.Element {
 		</section>
 	);
 }
-
-
-
-
